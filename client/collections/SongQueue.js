b0debca750dbc8cfa7f0ad1783c59462bd0d9342
misteryeo <andy.yeo@gmail.com>
2017-02-20 19:51:41 -0800

    Pass tests for SongQueue.js

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,8 +2,28 @@
 var SongQueue = Backbone.Collection.extend({
 
   model: SongModel,
-
+  
   initialize: function() {
-  }
+    this.on('add', function() {
+      if (this.models.length === 1) {
+        this.playFirst();
+      } else {
+        return;
+      }      
+    });
+    this.on('ended', this.removeAtEnd);
+    this.on('dequeue', this.removeAtEnd); // Mr Brodie is worried that dequeued actually means you can remove any song from the song at any location.
+  },
 
+  playFirst: function() {
+    this.at(0).play();
+  },
+
+  removeAtEnd: function() {
+    this.remove(this.at(0));
+    if (this.models.length > 0) {
+      this.playFirst();
+    }
+  }
+  
 });
