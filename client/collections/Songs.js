373d44e9f6c23d484da294b8bdb516256abea13f
Andy Yeo <andy.yeo@gmail.com>
2017-02-22 08:58:10 -0800

    Update server details on Songs file.

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,16 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
-  model: SongModel
+  model: SongModel,
+
+  url: 'http://parse.sfm8.hackreactor.com/mytunes/classes/songs',
+
+  initialize() {
+    this.fetch({
+      success: function(collection, response, options) {
+        this.set(response.results);
+      }.bind(this)
+    });
+  },
 
 });
